4befa9886c6a2cb5964efd10145f007d37bad075
Victor Silva <vitor1908@gmail.com>
2023-02-08 07:12:48 -0300

    feat: create success page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { GetStaticProps } from 'next'
+import Link from 'next/link'
 import { useKeenSlider } from 'keen-slider/react'
 import Stripe from 'stripe'
 
@@ -29,19 +30,20 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className='keen-slider'>
       {products.map((product) => {
         return (
-          <Product
+          <Link
             href={`/product/${product.id}`}
             key={product.id}
-            className='keen-slider__slide'
             prefetch={false}
           >
-            <Image src={product.imageUrl} width={520} height={480} alt='' />
+            <Product className='keen-slider__slide'>
+              <Image src={product.imageUrl} width={520} height={480} alt='' />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
