454a72275b12b2605ef1109cb2c5b6d1855b269c
Victor Silva <vitor1908@gmail.com>
2023-02-08 07:36:01 -0300

    feat: apply SEO good practices

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { GetStaticProps } from 'next'
 import Link from 'next/link'
+import Head from 'next/head'
 import { useKeenSlider } from 'keen-slider/react'
 import Stripe from 'stripe'
 
@@ -27,26 +28,32 @@ export default function Home({ products }: HomeProps) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className='keen-slider'>
-      {products.map((product) => {
-        return (
-          <Link
-            href={`/product/${product.id}`}
-            key={product.id}
-            prefetch={false}
-          >
-            <Product className='keen-slider__slide'>
-              <Image src={product.imageUrl} width={520} height={480} alt='' />
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
 
-              <footer>
-                <strong>{product.name}</strong>
-                <span>{product.price}</span>
-              </footer>
-            </Product>
-          </Link>
-        )
-      })}
-    </HomeContainer>
+      <HomeContainer ref={sliderRef} className='keen-slider'>
+        {products.map((product) => {
+          return (
+            <Link
+              href={`/product/${product.id}`}
+              key={product.id}
+              prefetch={false}
+            >
+              <Product className='keen-slider__slide'>
+                <Image src={product.imageUrl} width={520} height={480} alt='' />
+
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
+          )
+        })}
+      </HomeContainer>
+    </>
   )
 }
 
